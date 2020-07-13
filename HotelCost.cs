a5e9540ccb5476e45529a5ed628506449727c11b
Taylor Somers <tsomers@yandex.com>
2020-07-13 09:46:18 -0400

    Add logic to collect season and weekend info from user

--- HotelCost.cs
+++ HotelCost.cs
@@ -4,6 +4,13 @@ class HotelCost
 {
   static void Main()
   {
-    
+    Console.WriteLine("In what season are you booking a stay?");
+    string season = Console.ReadLine();
+
+    Console.WriteLine("On the weekend or a weeknight?");
+    string dayOfWeek = Console.ReadLine();
+
+    bool summer = season == "summer";
+    bool weekend = dayOfWeek == "weekend";
   }
 }
